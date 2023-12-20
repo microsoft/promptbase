27a6817b381c3d5ec34999a0ea713a7212cb42ac
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2023-12-20 09:08:36 -0500

    Thinking configs....

--- azureml/pipelines/configs.py
+++ azureml/pipelines/configs.py
@@ -8,3 +8,11 @@ class AMLConfig:
     workspace_name: str = str()
     resource_group: str = str()
     subscription_id: str = str()
+
+
+@dataclass
+class ZeroShotRunConfig:
+    base_experiment_name: str = str()
+    tags: Dict[str, str] = field(default_factory=dict)
+    mmlu_dataset: str = str()
+    default_compute_target: str = str()
