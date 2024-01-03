aad92737b2d03984495065875b01ec61e67011cc
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-01-03 13:03:16 -0500

    Reformat utils.py

--- src/promptbase/mmlu/utils.py
+++ src/promptbase/mmlu/utils.py
@@ -14,9 +14,11 @@ import numpy as np
 import requests
 from tqdm import tqdm
 
-logging.basicConfig(level = os.environ.get("LOGLEVEL", "WARN"))
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "WARN"))
 
-logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+logFormatter = logging.Formatter(
+    "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s"
+)
 rootLogger = logging.getLogger()
 
 fileHandler = logging.FileHandler("warnings.log")
@@ -35,14 +37,15 @@ def load_jsonl(file_name):
     # Load a jsonl file as list of objects
     with open(file_name) as f:
         lines = f.readlines()
-    
+
     return [json.loads(line) for line in lines if line]
+
 
 def dump_jsonl(objs, file_name):
     # Save a list/dict of objects to jsonl
     # If it is dict, we only save the values
 
-    with open(file_name, 'w') as f:
+    with open(file_name, "w") as f:
         if type(objs) is dict:
             for key in objs:
                 f.write(json.dumps(objs[key]) + "\n")
@@ -50,19 +53,24 @@ def dump_jsonl(objs, file_name):
             for obj in objs:
                 f.write(json.dumps(obj) + "\n")
 
+
 ################################################################################
 # String-Related
 ################################################################################
 
+
 def now_string():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-def random_string(length = 6):
-    return ''.join(random.choices(string.digits + string.ascii_letters, k=length))
+
+def random_string(length=6):
+    return "".join(random.choices(string.digits + string.ascii_letters, k=length))
+
 
 ################################################################################
 # Multithreading
 ################################################################################
+
 
 def run_batch_jobs(run_task, tasks, max_thread):
     """
@@ -72,9 +80,11 @@ def run_batch_jobs(run_task, tasks, max_thread):
     - max_thread: the number of thread we use
     """
     results = []
-    with concurrent.futures.ThreadPoolExecutor(max_thread) as executor, tqdm(total = len(tasks)) as pbar:
+    with concurrent.futures.ThreadPoolExecutor(max_thread) as executor, tqdm(
+        total=len(tasks)
+    ) as pbar:
         futures = [executor.submit(run_task, task) for task in tasks]
-        
+
         for future in concurrent.futures.as_completed(futures):
             pbar.update(1)
             try:
@@ -82,7 +92,7 @@ def run_batch_jobs(run_task, tasks, max_thread):
                 results.append(result)
             except Exception as e:
                 logging.exception("Error occurred during run_batch_jobs.")
-        
+
     return results
 
 
@@ -92,30 +102,37 @@ def run_batch_jobs(run_task, tasks, max_thread):
 
 openai_configs = types.SimpleNamespace()
 
-openai_configs.models = {
-}
+openai_configs.models = {}
 
 openai_configs.endpoints = {
     "openai-embeddings": {
-        "headers": {"Authorization": os.getenv("AZURE_OPENAI_API_KEY")}, 
+        "headers": {"Authorization": os.getenv("AZURE_OPENAI_API_KEY")},
         "url": os.getenv("AZURE_OPENAI_EMBEDDINGS_URL"),
     }
 }
 
-openai_configs.busy_message = ["temporarily unable to process your request", "The server had an error while processing your request. Sorry about that!", "Requests to the Creates a completion for the chat message Operation under Azure OpenAI API", "Requests to the Completions_Create Operation under Azure OpenAI API version", "Rate limit reached for", "exceeded call rate limit"]
+openai_configs.busy_message = [
+    "temporarily unable to process your request",
+    "The server had an error while processing your request. Sorry about that!",
+    "Requests to the Creates a completion for the chat message Operation under Azure OpenAI API",
+    "Requests to the Completions_Create Operation under Azure OpenAI API version",
+    "Rate limit reached for",
+    "exceeded call rate limit",
+]
 openai_configs.filtered_message = None
 
-def embed(text, model_name = "text-embedding-ada-002"):
+
+def embed(text, model_name="text-embedding-ada-002"):
     model_config = openai_configs.models[model_name]
     endpoint = openai_configs.endpoints[model_config["endpoint"]]
-    assert model_config["type"]  == "embedding"
+    assert model_config["type"] == "embedding"
     header = endpoint["headers"]
     url = endpoint["url"]
 
     data = {"input": text, "model": model_name}
-    
-    response = requests.post(url, headers=header, json=data, timeout=(30,600))
-    data = response.json()['data']
+
+    response = requests.post(url, headers=header, json=data, timeout=(30, 600))
+    data = response.json()["data"]
     if type(text) == str:
         return data[0]["embedding"]
     else:
@@ -123,19 +140,39 @@ def embed(text, model_name = "text-embedding-ada-002"):
         sorted_embeddings = [item["embedding"] for item in sorted_data]
         return sorted_embeddings
 
+
 def embed_batch(texts, model_name="text-embedding-ada-002", batch_size=100):
     embeddings = []
     # Split the input list into sublists of size batch_size
-    batches = np.array_split(texts, len(texts) // batch_size if len(texts) % batch_size == 0 else len(texts) // batch_size + 1)
+    batches = np.array_split(
+        texts,
+        len(texts) // batch_size
+        if len(texts) % batch_size == 0
+        else len(texts) // batch_size + 1,
+    )
     for batch in tqdm(batches):
-        batch = list(batch)  # Convert batch back to list if it isn't (depends on array_split output)
+        batch = list(
+            batch
+        )  # Convert batch back to list if it isn't (depends on array_split output)
         batch_embeddings = embed(batch, model_name)
         embeddings.extend(batch_embeddings)
     return embeddings
 
-def text_completion_impl(prompt, model="gpt-4-1106-preview", temperature=0, max_tokens=3500,
-                    top_p=1.0, logprobs=10, stop="<|diff_marker|>", echo=False, presence_penalty=0.0, frequency_penalty=0.0, 
-                    max_trial=100, **kwargs):
+
+def text_completion_impl(
+    prompt,
+    model="gpt-4-1106-preview",
+    temperature=0,
+    max_tokens=3500,
+    top_p=1.0,
+    logprobs=10,
+    stop="<|diff_marker|>",
+    echo=False,
+    presence_penalty=0.0,
+    frequency_penalty=0.0,
+    max_trial=100,
+    **kwargs,
+):
     """
     Performs text completion using the openai API with
     - prompt (str or array of str)
@@ -152,16 +189,22 @@ def text_completion_impl(prompt, model="gpt-4-1106-preview", temperature=0, max_
     s = requests.Session()
     if model_config["type"] == "chat":
         if type(prompt) is list and type(prompt[0]) is str:
-            assert len(prompt) == 1 # chat model only support 1 prompt at a time
+            assert len(prompt) == 1  # chat model only support 1 prompt at a time
             prompt = prompt[0]
         if type(prompt) is str:
             prompt = [{"role": "user", "content": prompt}]
     elif model_config["type"] == "completion":
         if type(prompt) is list and type(prompt[0]) is dict:
-            prompt = ''.join(f"<|im_start|>{message['role']}<|im_sep|>{message['content']}<|diff_marker|>" for message in prompt) + "<|im_start|>assistant"
+            prompt = (
+                "".join(
+                    f"<|im_start|>{message['role']}<|im_sep|>{message['content']}<|diff_marker|>"
+                    for message in prompt
+                )
+                + "<|im_start|>assistant"
+            )
     else:
         raise "Not supported"
-    
+
     for retry in range(max_trial):
         last_status_code = 0
         try:
@@ -178,33 +221,34 @@ def text_completion_impl(prompt, model="gpt-4-1106-preview", temperature=0, max_
                 "presence_penalty": presence_penalty,
                 "frequency_penalty": frequency_penalty,
                 "echo": echo,
-                "stop": stop
+                "stop": stop,
             }
-            headers = endpoint['headers']
+            headers = endpoint["headers"]
             if callable(headers):
                 headers = headers()
-            
-            url = endpoint['url']
-            
+
+            url = endpoint["url"]
+
             if model_config["type"] == "chat":
-                payload['messages'] = payload['prompt']
-                del payload['prompt'], payload['logprobs'], payload['echo']
+                payload["messages"] = payload["prompt"]
+                del payload["prompt"], payload["logprobs"], payload["echo"]
 
             logging.info("Request:" + str(payload))
-            r = s.post(url,  
-                headers = headers,
-                json = payload,
-                timeout = 200
-            )
-            
+            r = s.post(url, headers=headers, json=payload, timeout=200)
+
             last_response = r.text
             last_status_code = r.status_code
             logging.info(f"{last_status_code} Response:\n" + last_response)
-            if r.status_code == 400 and "The response was filtered due to the prompt triggering Azure OpenAI" in r.text and openai_configs.filtered_message is not None:
+            if (
+                r.status_code == 400
+                and "The response was filtered due to the prompt triggering Azure OpenAI"
+                in r.text
+                and openai_configs.filtered_message is not None
+            ):
                 row = json.loads(r.content)
-                row['text'] = openai_configs.filtered_message
-                row['finish_reason'] = "content_filter"
-                return {'response': {"choices":[row]}, 'success': False}
+                row["text"] = openai_configs.filtered_message
+                row["finish_reason"] = "content_filter"
+                return {"response": {"choices": [row]}, "success": False}
 
             if r.status_code == 200:
                 response = json.loads(r.content)
@@ -213,24 +257,25 @@ def text_completion_impl(prompt, model="gpt-4-1106-preview", temperature=0, max_
                     if response["choices"][k]["finish_reason"] == "content_filter":
                         response["choices"][k]["text"] = openai_configs.filtered_message
 
-                
                 if model_config["type"] == "chat":
                     for k in range(len(response["choices"])):
                         if "message" in response["choices"][k]:
-                            response["choices"][k]["text"] = response["choices"][k]["message"]["content"]
+                            response["choices"][k]["text"] = response["choices"][k][
+                                "message"
+                            ]["content"]
                             del response["choices"][k]["message"]
 
-                if (len(response["choices"]) == 1):
+                if len(response["choices"]) == 1:
                     text = response["choices"][0]["text"]
                 else:
                     text = []
                     for r in response["choices"]:
                         text.append(r["text"])
-                
-                return {'response': response, 'text': text, 'success': True}
+
+                return {"response": response, "text": text, "success": True}
         except Exception as e:
             logging.exception("Error occurred during HTTP calls in text_completion.")
-        
+
         filtered_warning = False
         for msg in openai_configs.busy_message:
             if msg in last_response:
@@ -238,20 +283,24 @@ def text_completion_impl(prompt, model="gpt-4-1106-preview", temperature=0, max_
 
         if not filtered_warning and last_status_code != 429:
             logging.warning(f"{last_status_code} Response:\n" + last_response)
-        
+
         if last_status_code not in [429, 500, 502, 503, 424]:
             break
-    return {'error': last_response, 'success': False}
+    return {"error": last_response, "success": False}
 
 
 def text_completion(**kwargs):
     result = text_completion_impl(**kwargs)
     if "log_file" in kwargs:
         message = "########## Prompt ##########\n"
-        message += str(kwargs['prompt']) + "\nmax_tokens=" + str(kwargs.get("max_tokens", 0)) + "\n"
+        message += (
+            str(kwargs["prompt"])
+            + "\nmax_tokens="
+            + str(kwargs.get("max_tokens", 0))
+            + "\n"
+        )
         message += "########## Response ##########\n"
-        message += result.get('text', "NONE") + "\n"
-        with open(kwargs['log_file'], "a") as f:
+        message += result.get("text", "NONE") + "\n"
+        with open(kwargs["log_file"], "a") as f:
             f.write(message)
     return result
-
