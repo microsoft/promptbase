8dca1245a5a1a76ebc67615300e83c9ca3c86a7e
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-01-03 09:33:56 -0500

    Make format_mmlu into a propery script

--- src/promptbase/format/format_mmlu.py
+++ src/promptbase/format/format_mmlu.py
@@ -1,24 +1,30 @@
-import json
-import os
+import argparse
 import csv
+import json
+import pathlib
 import uuid
 
 
-train_dir = "datasets/MMLU_all/auxiliary_train"
-dev_dir = "datasets/MMLU_all/dev"
-test_dir = "datasets/MMLU_all/test"
-val_dir = "datasets/MMLU_all/val"
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--mmlu_csv_dir", type=pathlib.Path, required=True)
+    parser.add_argument("--output_path", type=pathlib.Path, required=True)
+
+    args = parser.parse_args()
+
+    return args
 
 
 # Function to process a single CSV file and return a list of question dictionaries
-def process_csv_file(file_path, split_name):
+def process_csv_file(file_path: pathlib.Path, split_name):
     questions = []
     with open(file_path, "r", encoding="utf-8") as file:
         csv_reader = csv.reader(file)
         for i, row in enumerate(csv_reader):
             question_text, *answers, correct_answer = row
             answer_choices = {chr(65 + i): answer for i, answer in enumerate(answers)}
-            test_name = file_path.split("\\")[-1].replace(".csv", "")
+            test_name = file_path.stem
 
             question_dict = {
                 "question_number": f"{test_name}_{i}",
@@ -35,34 +41,26 @@ def process_csv_file(file_path, split_name):
     return questions
 
 
-all_questions = []
+def main(mmlu_csv_dir: pathlib.Path, output_path: pathlib.Path):
+    all_questions = []
 
-# List csv files in train_dir
-train_csv_list = os.listdir(train_dir)
-for train_csv in train_csv_list:
-    train_path = os.path.join(train_dir, train_csv)
-    questions = process_csv_file(train_path, "train")
-    all_questions.extend(questions)
-    print(json.dumps(questions[3], indent=4, ensure_ascii=False))
+    splits = dict(
+        train=mmlu_csv_dir / "auxiliary_train",
+        dev=mmlu_csv_dir / "dev",
+        test=mmlu_csv_dir / "test",
+        val=mmlu_csv_dir / "val",
+    )
 
-for dev_csv in os.listdir(dev_dir):
-    dev_path = os.path.join(dev_dir, dev_csv)
-    questions = process_csv_file(dev_path, "dev")
-    all_questions.extend(questions)
-    print(json.dumps(questions[3], indent=4, ensure_ascii=False))
+    for split_name, split_path in splits.items():
+        for csv_file in split_path.iterdir():
+            questions = process_csv_file(csv_file, split_name)
+            all_questions.extend(questions)
+            print(json.dumps(questions[3], indent=4, ensure_ascii=False))
 
-for test_csv in os.listdir(test_dir):
-    test_path = os.path.join(test_dir, test_csv)
-    questions = process_csv_file(test_path, "test")
-    all_questions.extend(questions)
-    print(json.dumps(questions[3], indent=4, ensure_ascii=False))
-
-for val_csv in os.listdir(val_dir):
-    val_path = os.path.join(val_dir, val_csv)
-    questions = process_csv_file(val_path, "val")
-    all_questions.extend(questions)
-    print(json.dumps(questions[3], indent=4, ensure_ascii=False))
+    with open(output_path, "w", encoding="utf-8") as json_file:
+        json.dump(all_questions, json_file, ensure_ascii=False, indent=4)
 
 
-with open("all_mmlu_questions.json", "w", encoding="utf-8") as json_file:
-    json.dump(all_questions, json_file, ensure_ascii=False, indent=4)
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args.mmlu_csv_dir, args.output_path)
