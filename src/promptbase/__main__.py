60376253d6987eed5235e3c54fe99524ed9802b2
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-01-09 10:50:11 -0500

    Hooking into main (and temporarily disabling the generation)

--- src/promptbase/__main__.py
+++ src/promptbase/__main__.py
@@ -75,8 +75,8 @@ def main():
     elif args.dataset == "mmlu":
         # Note that to run the MMLU tests, you will need to download the
         # data, and then use the 'format_mmlu.py' script
-        mmlu.generate(args.subject)
-        pass
+        #mmlu.generate(args.subject)
+        mmlu.evaluate_all(args.subject)
     else:
         raise ValueError(f"Bad dataset: {args.dataset}")
 
